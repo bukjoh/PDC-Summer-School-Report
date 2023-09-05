d845cc97ebecd14a237360a4099c3cc55c0252c2
Magnus Johansen <bukjoh.9@gmail.com>
2023-09-05 12:36:02 +0200

    Updated to new test case

--- run_mpi.sh
+++ run_mpi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -n $1 energy_storms_mpi 1000000 test_files/test_07_a1M_p5k_w1 test_files/test_07_a1M_p5k_w2 test_files/test_07_a1M_p5k_w3 test_files/test_07_a1M_p5k_w4
+srun -n $1 energy_storms_mpi 3000000 test_files/test_07_a1M_p5k_w1 test_files/test_07_a1M_p5k_w2 test_files/test_07_a1M_p5k_w3 test_files/test_07_a1M_p5k_w4
--- run_seq.sh
+++ run_seq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -n1 energy_storms_seq 1000000 test_files/test_07_a1M_p5k_w1 test_files/test_07_a1M_p5k_w2 test_files/test_07_a1M_p5k_w3 test_files/test_07_a1M_p5k_w4
+srun -n1 energy_storms_seq 3000000 test_files/test_07_a1M_p5k_w1 test_files/test_07_a1M_p5k_w2 test_files/test_07_a1M_p5k_w3 test_files/test_07_a1M_p5k_w4
